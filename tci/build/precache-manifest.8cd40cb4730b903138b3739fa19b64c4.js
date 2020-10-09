b1c482f42558087e9ecfc0a8b3ae03191bfb65bc
David Olmstead <david.lorne.olmstead@gmail.com>
2020-10-09 12:22:56 -0700

    Added production build of tci simulator alpha.

---
+++ tci/build/precache-manifest.8cd40cb4730b903138b3739fa19b64c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "529c315d53e98471f9fd7cfdbe6a76f8",
+    "url": "/tci/build/index.html"
+  },
+  {
+    "revision": "8d89b28db0cf84f811df",
+    "url": "/tci/build/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "8765bf9870f58aa893cd",
+    "url": "/tci/build/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8d89b28db0cf84f811df",
+    "url": "/tci/build/static/js/2.a9501f24.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/tci/build/static/js/2.a9501f24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8765bf9870f58aa893cd",
+    "url": "/tci/build/static/js/main.163de4ca.chunk.js"
+  },
+  {
+    "revision": "9bcec00546cf1414f3a4",
+    "url": "/tci/build/static/js/runtime-main.7328937e.js"
+  }
+]);
